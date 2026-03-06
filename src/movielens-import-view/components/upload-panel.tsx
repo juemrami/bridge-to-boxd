1f6050beab8922a5427183ed065de36dcb8bac64
juemrami <juemrami@gmail.com>
2026-03-05 20:06:34 -0800

    fix: make upload pseudo-buttons respect disabled prop

--- src/movielens-import-view/components/upload-panel.tsx
+++ src/movielens-import-view/components/upload-panel.tsx
@@ -1,4 +1,4 @@
-import type { Component } from "solid-js"
+import type { Component, JSX } from "solid-js"
 import { type UploadPanelProps } from "../index"
 
 export const UploadPanel: Component<UploadPanelProps> = (props) => {
@@ -10,6 +10,33 @@ export const UploadPanel: Component<UploadPanelProps> = (props) => {
 		chooseFile: "Choose a File"
 	} as const
 	const formatUploadSummaryText = (rows: number, issues: number) => `Summary: ${rows} staged rows • ${issues} issues`
+	const FileUploadInput = (
+		{ disabled, inputId, onUpload }: {
+			disabled?: boolean
+			inputId: string
+			onUpload: JSX.ChangeEventHandlerUnion<HTMLInputElement, Event>
+		}
+	) => (
+		<label for={inputId} class="">
+			<input
+				type="button"
+				value={displayText.chooseFile}
+				class="outlined-orange-button w-max"
+				disabled={disabled}
+				onClick={() => document.getElementById(inputId)?.click()}
+			/>
+			<input
+				class="w-0 h-0 opacity-0 absolute"
+				id={inputId}
+				type="file"
+				accept=".csv,text/csv"
+				aria-label={displayText.chooseFile}
+				inert
+				disabled={disabled}
+				onChange={onUpload}
+			/>
+		</label>
+	)
 	return (
 		<section class="my-3">
 			<h2 class="text-2xl font-bold mb-4">{displayText.title}</h2>
@@ -19,53 +46,35 @@ export const UploadPanel: Component<UploadPanelProps> = (props) => {
 					<p class="font-semibold mb-1">
 						{displayText.ratingsLabel}
 					</p>
-					<label for="ratings-file" class="">
-						<p class="outlined-orange-button w-max">{displayText.chooseFile}</p>
-						<input
-							class="w-0 h-0 opacity-0 absolute"
-							id="ratings-file"
-							type="file"
-							accept=".csv,text/csv"
-							onChange={props.onRatingsUpload}
-						/>
-					</label>
-					<p class="text-sm secondary-text">{props.renderUploadMeta(props.ratingsUpload())}</p>
+					<FileUploadInput
+						inputId="ratings-file"
+						onUpload={props.onRatingsUpload}
+					/>
+					<p class="text-sm secondary-text mt-1">{props.renderUploadMeta(props.ratingsUpload())}</p>
 				</div>
 
 				<div id="logs-upload" class={`${props.canUploadOptional() ? "" : "opacity-50"}`}>
 					<p class="font-semibold mb-1">
 						{displayText.logsLabel}
 					</p>
-					<label for="logs-file" class="">
-						<p class="outlined-orange-button w-max">{displayText.chooseFile}</p>
-						<input
-							class="w-0 h-0 opacity-0 absolute"
-							id="logs-file"
-							type="file"
-							accept=".csv,text/csv"
-							disabled={!props.canUploadOptional()}
-							onChange={props.onLogsUpload}
-						/>
-					</label>
-					<p class="text-sm secondary-text">{props.renderUploadMeta(props.logsUpload())}</p>
+					<FileUploadInput
+						inputId="logs-file"
+						onUpload={props.onLogsUpload}
+						disabled={!props.canUploadOptional()}
+					/>
+					<p class="text-sm secondary-text mt-1">{props.renderUploadMeta(props.logsUpload())}</p>
 				</div>
 
 				<div id="tags-upload" class={`${props.canUploadOptional() ? "" : "opacity-50"}`}>
 					<p class="font-semibold mb-1">
 						{displayText.tagsLabel}
 					</p>
-					<label for="tags-file" class="">
-						<p class="outlined-orange-button w-max">{displayText.chooseFile}</p>
-						<input
-							class="w-0 h-0 opacity-0 absolute"
-							id="tags-file"
-							type="file"
-							accept=".csv,text/csv"
-							disabled={!props.canUploadOptional()}
-							onChange={props.onTagsUpload}
-						/>
-					</label>
-					<p class="text-sm secondary-text">{props.renderUploadMeta(props.tagsUpload())}</p>
+					<FileUploadInput
+						inputId="tags-file"
+						onUpload={props.onTagsUpload}
+						disabled={!props.canUploadOptional()}
+					/>
+					<p class="text-sm secondary-text mt-1">{props.renderUploadMeta(props.tagsUpload())}</p>
 				</div>
 			</div>
 			<p id="upload-summary" class="text-sm">
